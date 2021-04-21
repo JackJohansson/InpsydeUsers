6b8bd8819377259113577a7f1e0ce0aaf0c9123f
Jack Johansson <jack.johansson1991@gmail.com>
2021-04-21 12:00:29 +0430

    Update Users.php

--- classes/Users.php
+++ classes/Users.php
@@ -1,335 +1,239 @@
 <?php
-
 /**
  * Class used to handle the user's data.
  *
  * @package Inpsyde
  */
 
-declare(strict_types=1);
-
 namespace Inpsyde;
+
+	// No direct access.
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'You shouldn\'t really be doing this.' );
+}
 
 /**
  * Class Users
  *
  * @package Inpsyde
  */
-class Users
-{
-    /**
-     * Holds the error code for the last ocurred error.
-     *
-     * @var string
-     */
-    private static string $lastErrorCode = '';
-    /**
-     * Holds the last occurred error.
-     *
-     * @var string $lastErrorMessage
-     */
-    private static string $lastErrorMessage = '';
+class Users {
 
-    /**
-     * Returns the last error code.
-     * @return string
-     */
-    public function errorCode(): string
-    {
-        return self::$lastErrorCode;
-    }
+	/**
+	 * Flush all the users from the cache.
+	 */
+	public function flush_all() {
+		$flush_users   = delete_transient( 'inpsyde_users' );
+		$flush_details = delete_transient( 'inpsyde_user_details' );
 
-    /**
-     * Returns the last error message.
-     * @return string
-     */
-    public function errorMessage(): string
-    {
-        return self::$lastErrorMessage;
-    }
+		if ( ! $flush_users ) {
+			return new \WP_Error( 'inpsyde_flush_failed', esc_html__( 'Could not flush the user cache. This is mostly because the results were already flushed.', 'inpsyde-users' ) );
+		}
 
-    /**
-     * Flush all the users from the cache.
-     */
-    public function flushAll(): bool
-    {
+		return true;
+	}
 
-        $flushUsers = delete_transient('inpsyde_users');
-        $flushDetails = delete_transient('inpsyde_user_details');
+	/**
+	 * Remove a single user from the cache.
+	 *
+	 * @param int $user_id The id of user which we want its data flushed.
+	 *
+	 * @return bool|\WP_Error The result of flushing, or an error object
+	 */
+	public function flush_user( int $user_id ) {
+		$cache = get_transient( 'inpsyde_user_details' );
 
-        if (! $flushUsers) {
-            $this->setError(
-                'inpsyde_flush_failed',
-                esc_html__(
-                    'Could not flush the user cache. This is mostly because the results were already flushed.',
-                    'inpsyde-users'
-                )
-            );
-            return false;
-        }
+		if ( false === $cache || ! isset( $cache[ $user_id ] ) ) {
+			return new \WP_Error( 'inpsyde_flush_failed', esc_html__( 'This user ID is not cached yet.', 'inpsyde-users' ) );
+		}
 
-        return true;
-    }
+		// Remove the current user.
+		unset( $cache[ $user_id ] );
 
-    /**
-     * Set the last error message and code.
-     * @param string $code
-     * @param string $message
-     */
-    private function setError(string $code, string $message)
-    {
+		// Save.
+		$save_result = set_transient( 'inpsyde_user_details', $cache, absint( apply_filters( 'inpsyde_transient', 3600 ) ) );
 
-        self::$lastErrorCode = $code;
-        self::$lastErrorMessage = $message;
-    }
+		// If saving has failed.
+		if ( ! $save_result ) {
+			return new \WP_Error( 'inpsyde_flush_failed', esc_html__( 'Could not save the cache into database. Please try again later.', 'inpsyde-users' ) );
+		}
 
-    /**
-     * Remove a single user from the cache.
-     *
-     * @param int $userId The id of user which we want its data flushed.
-     *
-     * @return bool The result of flushing.
-     */
-    public function flushUser(int $userId): bool
-    {
+		return true;
+	}
 
-        $cache = get_transient('inpsyde_user_details');
+	/**
+	 * List all the users and their information.
+	 *
+	 * @return \WP_Error|array An instance of WP_Error on failure, or an array of user data.
+	 */
+	public function list_users() {
 
-        if (false === $cache || ! isset($cache[ $userId ])) {
-            $this->setError(
-                'inpsyde_flush_failed',
-                esc_html__('This user ID is not cached yet.', 'inpsyde-users')
-            );
-            return false;
-        }
+		// Check the cache first.
+		$cached_users = get_transient( 'inpsyde_users' );
 
-        // Remove the current user.
-        unset($cache[ $userId ]);
+		// This is already properly formatted.
+		if ( false !== $cached_users ) {
+			return $cached_users;
+		}
 
-        // Apply filters.
-        $transient = absint(apply_filters('inpsyde_transient', 3600));
+		// Make a new request to the API.
+		$request_decoded = $this->get_results( Restful::REST_USERS_ROUTE );
 
-        // Save.
-        $saveResult = set_transient('inpsyde_user_details', $cache, $transient);
+		// An error occurred!
+		if ( is_wp_error( $request_decoded ) ) {
+			return $request_decoded;
+		}
 
-        // If saving has failed.
-        if (! $saveResult) {
-            $this->setError(
-                'inpsyde_flush_failed',
-                esc_html__(
-                    'Could not save the cache into database. Please try again later.',
-                    'inpsyde-users'
-                )
-            );
-            return false;
-        }
-        return true;
-    }
+		$response = array(
+			'meta' => array(
+				'page'    => 1,
+				'pages'   => max( absint( count( $request_decoded ) / 10 ), 1 ),
+				'perpage' => - 1,
+				'total'   => count( $request_decoded ),
+				'sort'    => 'asc',
+				'field'   => 'ID',
+			),
+		);
 
-    /**
-     * List all the users and their information.
-     *
-     * @return array An empty array on failure, or an array of user data.
-     */
-    public function listUsers(): array
-    {
+		// Construct the response.
+		foreach ( $request_decoded as $user ) {
+			$response['data'][] = array(
+				'ID'       => $user['id'] ?? 0,
+				'Name'     => $user['name'] ?? '',
+				'Username' => $user['username'] ?? '',
+				'Email'    => $user['email'] ?? '',
+				'Phone'    => $user['phone'] ?? '',
+				'Website'  => $user['website'] ?? '',
+				'Actions'  => $user['id'] ?? 0,
+			);
+		}
 
-        // Check the cache first.
-        $cachedUsers = get_transient('inpsyde_users');
+		// Cache for an hour. let's not store the entire response so we can write more unnecessary code for demonstration purposes ONLY.
+		set_transient( 'inpsyde_users', $response, absint( apply_filters( 'inpsyde_transient', 3600 ) ) );
 
-        // This is already properly formatted.
-        if (false !== $cachedUsers) {
-            return $cachedUsers;
-        }
+		// Return the results.
+		return $response;
+	}
 
-        // Make a new request to the API.
-        $requestDecoded = $this->fetchResults(Restful::REST_USERS_ROUTE);
+	/**
+	 * Fetch the results from an specific endpoint, and
+	 * decode them.
+	 *
+	 * @param string $endpoint The api endpoint used to fetch data from.
+	 *
+	 * @return array|\WP_Error An array of user data, or an object of WP_Error on failure.
+	 */
+	public function get_results( string $endpoint ) {
 
-        // An error occurred!
-        if (empty($requestDecoded)) {
-            return [];
-        }
+		// Make the request to the API.
+		$request_body = $this->make_request( $endpoint );
 
-        $response = [
-            'meta' => [
-                'page' => 1,
-                'pages' => max(absint(count($requestDecoded) / 10), 1),
-                'perpage' => -1,
-                'total' => count($requestDecoded),
-                'sort' => 'asc',
-                'field' => 'ID',
-            ],
-        ];
+		// if the result is empty.
+		if ( empty( $request_body ) ) {
+			return new \WP_Error( 'inpsyde_empty_response', esc_html__( 'The API has returned an empty response. Please try again later.', 'inpsyde-users' ) );
+		}
 
-        // Construct the response.
-        foreach ($requestDecoded as $user) {
-            $response['data'][] = [
-                'ID' => $user['id'] ?? 0,
-                'Name' => $user['name'] ?? '',
-                'Username' => $user['username'] ?? '',
-                'Email' => $user['email'] ?? '',
-                'Phone' => $user['phone'] ?? '',
-                'Website' => $user['website'] ?? '',
-                'Actions' => $user['id'] ?? 0,
-            ];
-        }
+		// Decode the json.
+		try {
+			$request_decoded = json_decode( $request_body, true, 512, JSON_THROW_ON_ERROR );
+		} catch ( \JsonException $e ) {
+			// Return the json error.
+			return new \WP_Error( 'inpsyde_json_error', $e->getMessage() );
+		}
 
-        // Cache for an hour. let's not store the entire response so we can write more unnecessary code for demonstration purposes ONLY.
-        set_transient('inpsyde_users', $response, absint(apply_filters('inpsyde_transient', 3600)));
+		return $request_decoded;
+	}
 
-        // Return the results.
-        return $response;
-    }
+	/**
+	 * Make a request to the external API, and return the string.
+	 *
+	 * @param string $endpoint The endpoint to connect to.
+	 *
+	 * @return string|\WP_Error The response body.
+	 */
+	public function make_request( string $endpoint ) {
 
-    /**
-     * Fetch the results from an specific endpoint, and
-     * decode them.
-     *
-     * @param string $endpoint The api endpoint used to fetch data from.
-     *
-     * @return array An array of user data, or an empty array on failure.
-     */
-    public function fetchResults(string $endpoint): array
-    {
+		// Make a new request to the API.
+		$wp_request = wp_safe_remote_get( API_ROOT . $endpoint );
 
-        // Make the request to the API.
-        $requestBody = $this->makeRequest($endpoint);
+		// Let the API handle the error.
+		if ( is_wp_error( $wp_request ) ) {
+			return $wp_request;
+		}
 
-        // if the result is empty.
-        if (empty($requestBody)) {
-            $this->setError(
-                'inpsyde_empty_response',
-                esc_html__(
-                    'The API has returned an empty response. Please try again later.',
-                    'inpsyde-users'
-                )
-            );
-            return [];
-        }
+		return wp_remote_retrieve_body( $wp_request );
+	}
 
-        // Decode the json.
-        try {
-            $requestDecoded = json_decode($requestBody, true, 512, JSON_THROW_ON_ERROR);
-        } catch (\JsonException $exception) {
-            // Return the json error.
-            $this->setError('inpsyde_json_error', $exception->getMessage());
-            return [];
-        }
+	/**
+	 * Get a user's detailed information from its ID.
+	 *
+	 * @param int $user_id The user id of whom we trying to get data.
+	 *
+	 * @return \WP_Error|array An array of user data, or an object of WP_Error on failure.
+	 */
+	public function user_info( int $user_id ) {
+		/**
+		 * We could have simply stored the entire JSON response, but for the
+		 * sake of the demonstration, let's make a new request for each
+		 * user. Why? Because most proper APIs store the users' details
+		 * on a different endpoint, so we need to make a new request for
+		 * each user.
+		 */
 
-        return $requestDecoded;
-    }
+		// Check the cache.
+		$cached_details = get_transient( 'inpsyde_user_details' );
 
-    /**
-     * Make a request to the external API, and return the string.
-     *
-     * @param string $endpoint The endpoint to connect to.
-     *
-     * @return string|\WP_Error The response body.
-     */
-    public function makeRequest(string $endpoint): string
-    {
+		// Check if this specific user id is cached. Otherwise, fetch.
+		if ( ! isset( $cached_details[ $user_id ] ) ) {
 
-        // Make a new request to the API.
-        $wp_request = wp_safe_remote_get(API_ROOT . $endpoint);
+			// No cache :( Time to make a request.
+			$request_decoded = $this->get_results( Restful::REST_USERS_ROUTE );
 
-        // Let the API handle the error.
-        if (is_wp_error($wp_request)) {
-            $this->setError($wp_request->get_error_code(), $wp_request->get_error_message());
-            return '';
-        }
+			// An error occurred!
+			if ( is_wp_error( $request_decoded ) ) {
+				return $request_decoded;
+			}
 
-        return wp_remote_retrieve_body($wp_request);
-    }
+			// Find the user details if it exists in the array.
+			$array_index = array_search( $user_id, array_column( $request_decoded, 'id' ), true );
 
-    /**
-     * Get a user's detailed information from its ID.
-     *
-     * @param int $userId The user id of whom we trying to get data.
-     *
-     * @return array An array of user data, or an empty array on failure.
-     */
-    public function userInfo(int $userId): array
-    {
-        /**
-         * We could have simply stored the entire JSON response, but for the
-         * sake of the demonstration, let's make a new request for each
-         * user. Why? Because most proper APIs store the users' details
-         * on a different endpoint, so we need to make a new request for
-         * each user.
-         */
+			// If the user id is invalid.
+			if ( false === $array_index ) {
+				return new \WP_Error( 'inpsyde_invalid_id', esc_html__( 'The requested user ID is not valid.', 'inpsyde-users' ) );
+			}
 
-        // Check the cache.
-        $cachedDetails = get_transient('inpsyde_user_details');
+			// Cache the results.
+			$cached_details[ $user_id ] = array(
+				'ID'       => isset( $request_decoded[ $array_index ]['id'] ) ? absint( $request_decoded[ $array_index ]['id'] ) : '',
+				'name'     => isset( $request_decoded[ $array_index ]['name'] ) ? sanitize_text_field( $request_decoded[ $array_index ]['name'] ) : '',
+				'username' => isset( $request_decoded[ $array_index ]['username'] ) ? sanitize_user( $request_decoded[ $array_index ]['username'] ) : '',
+				'company'  => isset( $request_decoded[ $array_index ]['company'] ) ? sanitize_user( $request_decoded[ $array_index ]['company']['name'] ) : '',
+				'email'    => isset( $request_decoded[ $array_index ]['email'] ) ? sanitize_email( $request_decoded[ $array_index ]['email'] ) : '',
+				'phone'    => isset( $request_decoded[ $array_index ]['phone'] ) ? sanitize_text_field( $request_decoded[ $array_index ]['phone'] ) : '',
+				'city'     => isset( $request_decoded[ $array_index ]['address'] ) ? sanitize_text_field( $request_decoded[ $array_index ]['address']['city'] ) : '',
+				'location' => isset( $request_decoded[ $array_index ]['address'] ) ? sanitize_text_field( $request_decoded[ $array_index ]['address']['street'] ) : '',
+				'website'  => isset( $request_decoded[ $array_index ]['website'] ) ? esc_url_raw( $request_decoded[ $array_index ]['website'] ) : '',
+				'avatar'   => get_avatar_url( $request_decoded[ $array_index ]['email'] ),
+			);
+			// Cache the results.
+			set_transient( 'inpsyde_user_details', $cached_details, absint( apply_filters( 'inpsyde_transient', 3600 ) ) );
+		}
 
-        // Check if this specific user id is cached. Otherwise, fetch.
-        if (! isset($cachedDetails[ $userId ])) {
-            // No cache :( Time to make a request.
-            $requestDecoded = $this->fetchResults(Restful::REST_USERS_ROUTE);
+		// Escape the results and output.
+		$response = array(
+			'ID'       => absint( $cached_details[ $user_id ]['ID'] ),
+			'name'     => esc_html( $cached_details[ $user_id ]['name'] ),
+			'username' => esc_html( $cached_details[ $user_id ]['username'] ),
+			'company'  => esc_html( $cached_details[ $user_id ]['company'] ),
+			'email'    => esc_html( $cached_details[ $user_id ]['email'] ),
+			'phone'    => esc_html( $cached_details[ $user_id ]['phone'] ),
+			'city'     => esc_html( $cached_details[ $user_id ]['city'] ),
+			'location' => esc_html( $cached_details[ $user_id ]['location'] ),
+			'website'  => esc_html( $cached_details[ $user_id ]['website'] ),
+			'avatar'   => esc_url( $cached_details[ $user_id ]['avatar'] ),
+		);
 
-            // An error occurred!
-            if (empty($requestDecoded)) {
-                return [];
-            }
-
-            // Find the user details if it exists in the array.
-            $arrayIndex = array_search($userId, array_column($requestDecoded, 'id'), true);
-
-            // If the user id is invalid.
-            if (false === $arrayIndex) {
-                $this->setError(
-                    'inpsyde_invalid_id',
-                    esc_html__('The requested user ID is not valid.', 'inpsyde-users')
-                );
-                return [];
-            }
-
-            // Cache the results.
-            $cachedDetails[$userId] = $this->buildUserData($requestDecoded[ $arrayIndex ]);
-
-            // Apply user filters.
-            $transient = absint(apply_filters('inpsyde_transient', 3600));
-            // Cache the results.
-            set_transient('inpsyde_user_details', $cachedDetails, $transient);
-        }
-
-        // Escape the results and output.
-        $response = [
-            'ID' => absint($cachedDetails[ $userId ][ 'ID']),
-            'name' => esc_html($cachedDetails[ $userId ][ 'name']),
-            'username' => esc_html($cachedDetails[ $userId ][ 'username']),
-            'company' => esc_html($cachedDetails[ $userId ][ 'company']),
-            'email' => esc_html($cachedDetails[ $userId ][ 'email']),
-            'phone' => esc_html($cachedDetails[ $userId ][ 'phone']),
-            'city' => esc_html($cachedDetails[ $userId ][ 'city']),
-            'location' => esc_html($cachedDetails[ $userId ][ 'location']),
-            'website' => esc_html($cachedDetails[ $userId ][ 'website']),
-            'avatar' => esc_url($cachedDetails[ $userId ][ 'avatar']),
-        ];
-
-        // Return the results.
-        return apply_filters('inpsyde_user_details', $response);
-    }
-
-    /**
-     * Build user's data based on a decoded json response.
-     * @param array $data
-     *
-     * @return array
-     */
-    public function buildUserData(array $data): array
-    {
-        $location = isset($data['address']) ? sanitize_text_field($data['address']['street']) : '';
-        return [
-            'ID' => isset($data['id']) ? absint($data['id']) : '',
-            'name' => isset($data['name']) ? sanitize_text_field($data['name']) : '',
-            'username' => isset($data['username']) ? sanitize_user($data['username']) : '',
-            'company' => isset($data['company']) ? sanitize_user($data['company']['name']) : '',
-            'email' => isset($data['email']) ? sanitize_email($data['email']) : '',
-            'phone' => isset($data['phone']) ? sanitize_text_field($data['phone']) : '',
-            'city' => isset($data['address']) ? sanitize_text_field($data['address']['city']) : '',
-            'location' => $location,
-            'website' => isset($data['website']) ? esc_url_raw($data['website']) : '',
-            'avatar' => get_avatar_url($data['email']),
-        ];
-    }
+		// Return the results.
+		return apply_filters( 'inpsyde_user_details', $response );
+	}
 }
