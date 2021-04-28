4a9d31f2d2409c0c594f492d046c02f9357b75bc
Jack Johansson <jack.johansson1991@gmail.com>
2021-04-28 14:48:12 +0430

    Updated line separators

--- inpsyde.php
+++ inpsyde.php
@@ -3,7 +3,7 @@
  * Plugin Name:       InpsydeUsers
  * Plugin URI:        https://github.com/JackJohansson/Inpsyde-Project
  * Description:       A sample project assigned by Inpsyde gmbh
- * Version:           1.0.1
+ * Version:           1.0.2
  * Requires at least: 5.2
  * Requires PHP:      7.4
  * Author:            Jack Johansson
