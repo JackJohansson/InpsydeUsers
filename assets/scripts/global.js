e934ded9724174e9fffb5abec6e7e1cdc973aff5
Jack Johansson <jack.johansson1991@gmail.com>
2021-04-28 13:05:25 +0430

    Updated structures, included missing files

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -1,297 +1,299 @@
 'use strict';
 (function ( $ ) {
 
-	/**
-	 * Get the details for a single user and fill
-	 * the data.
-	 *
-	 * @param userId
-	 */
-	function inpsydeGetUserDetails ( userId ) {
+    /**
+     * Get the details for a single user and fill
+     * the data.
+     *
+     * @param userId
+     */
+    function inpsydeGetUserDetails( userId )
+    {
 
-		let tableWrapper = $( '#inpsyde-table-wrapper' );
-		let userDetails  = $( '#inpsyde-user-details' );
+        let tableWrapper = $('#inpsyde-table-wrapper');
+        let userDetails  = $('#inpsyde-user-details');
 
-		// Send an ajax request
-		$.ajax(
-			{
-				url: inpsyde.rest_url.user_details,
-				method: 'GET',
-				data: { id: userId, nonce: inpsyde.nonce },
-				dataType: 'json',
-				beforeSend: function ( jqXHR, settings ) {
-					jqXHR.overrideMimeType( "text/plain; charset=x-user-defined" );
-					jqXHR.setRequestHeader( 'X-WP-Nonce', inpsyde.nonce );
+        // Send an ajax request
+        $.ajax(
+            {
+                url: inpsyde.rest_url.base + inpsyde.rest_url.user_details,
+                method: 'GET',
+                data: { id: userId, nonce: inpsyde.nonce },
+                dataType: 'json',
+                beforeSend: function ( jqXHR, settings ) {
+                    jqXHR.overrideMimeType("text/plain; charset=x-user-defined");
+                    jqXHR.setRequestHeader('X-WP-Nonce', inpsyde.nonce);
 
-					// Block the table
-					tableWrapper.block( {
-						message: '<div class="blockui "><span>' + inpsyde.i18n.processing + '</span><span><div class="spinner spinner--v2 spinner--primary "></div></span></div>',
-						css: {
-							border: '0px',
-							backgroundColor: 'transparent',
-							textAlign: 'center',
-							width: 'auto',
-						},
-						overlayCSS: {
-							backgroundColor: '#000000',
-							opacity: 0.1,
-							cursor: 'wait'
-						}
-					} );
-				},
-				success: function ( data, textStatus, jqXHR ) {
+                    // Block the table
+                    tableWrapper.block({
+                        message: '<div class="blockui "><span>' + inpsyde.i18n.processing + '</span><span><div class="spinner spinner--v2 spinner--primary "></div></span></div>',
+                        css: {
+                            border: '0px',
+                            backgroundColor: 'transparent',
+                            textAlign: 'center',
+                            width: 'auto',
+                        },
+                        overlayCSS: {
+                            backgroundColor: '#000000',
+                            opacity: 0.1,
+                            cursor: 'wait'
+                        }
+                    });
+                },
+                success: function ( data, textStatus, jqXHR ) {
 
-					// Check if the server's response vas valid
-					if ( false === data.success ) {
-						swal.fire( inpsyde.i18n.error, data.message, 'error' );
-						return;
-					}
+                    // Check if the server's response vas valid
+                    if ( false === data.success ) {
+                        swal.fire(inpsyde.i18n.error, data.message, 'error');
+                        return;
+                    }
 
-					// Render the details
-					$( '#inpsyde-name' ).text( data.name );
-					$( '#inpsyde-user-company' ).text( data.company );
-					$( '#inpsyde-user-email' ).text( data.email );
-					$( '#inpsyde-user-phone' ).text( data.phone );
-					$( '#inpsyde-user-address' ).text( data.location );
-					$( '#inpsyde-user-website' ).text( data.website );
-					$( '#inpsyde-user-location' ).text( data.city );
-					$( '#inpsyde-refresh-details' ).data( 'id', data.ID );
-					$( '#inpsyde-user-avatar' ).attr( { src: data.avatar, alt: data.name } );
+                    // Render the details
+                    $('#inpsyde-name').text(data.name);
+                    $('#inpsyde-user-company').text(data.company);
+                    $('#inpsyde-user-email').text(data.email);
+                    $('#inpsyde-user-phone').text(data.phone);
+                    $('#inpsyde-user-address').text(data.location);
+                    $('#inpsyde-user-website').text(data.website);
+                    $('#inpsyde-user-location').text(data.city);
+                    $('#inpsyde-refresh-details').data('id', data.ID);
+                    $('#inpsyde-user-avatar').attr({ src: data.avatar, alt: data.name });
 
-					// Show the details
-					userDetails.fadeIn();
+                    // Show the details
+                    userDetails.fadeIn();
 
-				},
-				error: function ( jqXHR, textStatus, errorThrown ) {
-					// Show the error
-					swal.fire( inpsyde.i18n.error, jqXHR.responseText, 'error' );
-					// Hide the results
-					userDetails.fadeOut();
-				},
-				complete: function ( jqXHR, textStatus ) {
-					// Unblock the table
-					tableWrapper.unblock();
-				}
-			}
-		);
-	}
+                },
+                error: function ( jqXHR, textStatus, errorThrown ) {
+                    // Show the error
+                    swal.fire(inpsyde.i18n.error, jqXHR.responseText, 'error');
+                    // Hide the results
+                    userDetails.fadeOut();
+                },
+                complete: function ( jqXHR, textStatus ) {
+                    // Unblock the table
+                    tableWrapper.unblock();
+                }
+            }
+        );
+    }
 
-	/**
-	 * Initiate and fill the datatable with users' data.
-	 *
-	 */
-	function inpsydeFillDatatable ( reset = false ) {
+    /**
+     * Initiate and fill the datatable with users' data.
+     *
+     */
+    function inpsydeFillDatatable( reset = false )
+    {
 
-		// Initialize the datatable and fill the fields
-		let table = $( '#inpsyde-table' );
+        // Initialize the datatable and fill the fields
+        let table = $('#inpsyde-table');
 
-		// If there's need to be a rest
-		if ( reset ) {
-			table.DataTable().destroy();
-		}
+        // If there's need to be a rest
+        if ( reset ) {
+            table.DataTable().destroy();
+        }
 
-		table.DataTable( {
-			responsive: true,
-			searchDelay: 500,
-			processing: true,
-			ajax: {
-				url: inpsyde.rest_url.users,
-				type: 'GET',
-				data: {
-					pagination: {
-						perpage: 10,
-					}
-				},
-				beforeSend: function ( xhr ) {
-					xhr.setRequestHeader( 'X-WP-Nonce', inpsyde.nonce );
-				},
-				error: function ( xhr, error, code ) {
-					swal.fire( inpsyde.i18n.error, error, 'error' );
-				},
-				dataSrc: function ( json ) {
-					if ( false === json.success ) {
-						// Show an alert
-						swal.fire( inpsyde.i18n.error, json.message, 'error' );
-						return [];
-					} else {
-						return json.data;
-					}
-				}
-			},
-			columns: [
-				{ data: 'ID' },
-				{ data: 'Name' },
-				{ data: 'Username' },
-				{ data: 'Email' },
-				{ data: 'Phone' },
-				{ data: 'Website' },
-				{ data: 'Actions', responsivePriority: -1 },
-			],
-			columnDefs: [
-				{
-					targets: -1,
-					title: 'Actions',
-					orderable: false,
-					render: function ( data, type, full, meta ) {
-						return `
+        table.DataTable({
+            responsive: true,
+            searchDelay: 500,
+            processing: true,
+            ajax: {
+                url: inpsyde.rest_url.base + inpsyde.rest_url.users,
+                type: 'GET',
+                data: {
+                    pagination: {
+                        perpage: 10,
+                    }
+                },
+                beforeSend: function ( xhr ) {
+                    xhr.setRequestHeader('X-WP-Nonce', inpsyde.nonce);
+                },
+                error: function ( xhr, error, code ) {
+                    swal.fire(inpsyde.i18n.error, error, 'error');
+                },
+                dataSrc: function ( json ) {
+                    if ( false === json.success ) {
+                        // Show an alert
+                        swal.fire(inpsyde.i18n.error, json.message, 'error');
+                        return [];
+                    } else {
+                        return json.data;
+                    }
+                }
+            },
+            columns: [
+                { data: 'ID' },
+                { data: 'Name' },
+                { data: 'Username' },
+                { data: 'Email' },
+                { data: 'Phone' },
+                { data: 'Website' },
+                { data: 'Actions', responsivePriority: -1 },
+            ],
+            columnDefs: [
+                {
+                    targets: -1,
+                    title: 'Actions',
+                    orderable: false,
+                    render: function ( data, type, full, meta ) {
+                        return `
                         <a href="#" data-userid="` + data + `" class="btn btn-label-primary inpsyde-get-user-details">
                           <i class="flaticon-eye"></i>` + inpsyde.i18n.view + `
                         </a>`;
-					},
-				}
-			],
-		} );
-	}
+                    },
+            }
+            ],
+        });
+    }
 
-	// On page load, fill the datatable.
-	inpsydeFillDatatable();
+    // On page load, fill the datatable.
+    inpsydeFillDatatable();
 
-	/**
-	 * Fetch user's details
-	 *
-	 */
-	$( '#inpsyde-table-wrapper' ).on( 'click', '.inpsyde-get-user-details', function ( e ) {
-		e.preventDefault();
+    /**
+     * Fetch user's details
+     *
+     */
+    $('#inpsyde-table-wrapper').on('click', '.inpsyde-get-user-details', function ( e ) {
+        e.preventDefault();
 
-		// Get the user's ID
-		let userId = $( this ).data( 'userid' );
+        // Get the user's ID
+        let userId = $(this).data('userid');
 
-		// Fetch and fill
-		inpsydeGetUserDetails( userId );
+        // Fetch and fill
+        inpsydeGetUserDetails(userId);
 
-	} );
+    });
 
-	/**
-	 * Flush the user table
-	 */
-	$( '#inpsyde-flush-users' ).on( 'click', function ( e ) {
+    /**
+     * Flush the user table
+     */
+    $('#inpsyde-flush-users').on('click', function ( e ) {
 
-		// Prevent any actions
-		e.preventDefault();
+        // Prevent any actions
+        e.preventDefault();
 
-		let tableWrapper = $( '#inpsyde-table_wrapper' );
+        let tableWrapper = $('#inpsyde-table_wrapper');
 
-		// Send an ajax request
-		$.ajax(
-			{
-				url: inpsyde.rest_url.users_flush,
-				method: 'DELETE',
-				data: { nonce: inpsyde.nonce },
-				dataType: 'json',
-				beforeSend: function ( jqXHR, settings ) {
+        // Send an ajax request
+        $.ajax(
+            {
+                url: inpsyde.rest_url.base + inpsyde.rest_url.users_flush,
+                method: 'DELETE',
+                data: { nonce: inpsyde.nonce },
+                dataType: 'json',
+                beforeSend: function ( jqXHR, settings ) {
 
-					jqXHR.overrideMimeType( "text/plain; charset=x-user-defined" );
-					jqXHR.setRequestHeader( 'X-WP-Nonce', inpsyde.nonce );
+                    jqXHR.overrideMimeType("text/plain; charset=x-user-defined");
+                    jqXHR.setRequestHeader('X-WP-Nonce', inpsyde.nonce);
 
-					// Block the table
-					tableWrapper.block( {
-						message: '<div class="blockui "><span>' + inpsyde.i18n.processing + '</span><span><div class="spinner spinner--v2 spinner--primary "></div></span></div>',
-						css: {
-							border: '0px',
-							backgroundColor: 'transparent',
-							textAlign: 'center',
-							width: 'auto',
-						},
-						overlayCSS: {
-							backgroundColor: '#000000',
-							opacity: 0.1,
-							cursor: 'wait'
-						}
-					} );
-				},
-				success: function ( data, textStatus, jqXHR ) {
+                    // Block the table
+                    tableWrapper.block({
+                        message: '<div class="blockui "><span>' + inpsyde.i18n.processing + '</span><span><div class="spinner spinner--v2 spinner--primary "></div></span></div>',
+                        css: {
+                            border: '0px',
+                            backgroundColor: 'transparent',
+                            textAlign: 'center',
+                            width: 'auto',
+                        },
+                        overlayCSS: {
+                            backgroundColor: '#000000',
+                            opacity: 0.1,
+                            cursor: 'wait'
+                        }
+                    });
+                },
+                success: function ( data, textStatus, jqXHR ) {
 
-					// Check if the server's response vas valid
-					if ( false === data.success ) {
-						swal.fire( inpsyde.i18n.error, data.message, 'error' );
-						return;
-					}
+                    // Check if the server's response vas valid
+                    if ( false === data.success ) {
+                        swal.fire(inpsyde.i18n.error, data.message, 'error');
+                        return;
+                    }
 
-					// Fetch a fresh copy
-					inpsydeFillDatatable( true );
+                    // Fetch a fresh copy
+                    inpsydeFillDatatable(true);
 
-				},
-				error: function ( jqXHR, textStatus, errorThrown ) {
-					// Show the error
-					swal.fire( inpsyde.i18n.error, jqXHR.responseText, 'error' );
-				},
-				complete: function ( jqXHR, textStatus ) {
-					// Unblock the table
-					tableWrapper.unblock();
-				}
-			}
-		);
+                },
+                error: function ( jqXHR, textStatus, errorThrown ) {
+                    // Show the error
+                    swal.fire(inpsyde.i18n.error, jqXHR.responseText, 'error');
+                },
+                complete: function ( jqXHR, textStatus ) {
+                    // Unblock the table
+                    tableWrapper.unblock();
+                }
+            }
+        );
 
-	} );
+    });
 
-	/**
-	 * Refresh the user's details
-	 *
-	 */
-	$( '#inpsyde-refresh-details' ).on( 'click', function ( e ) {
+    /**
+     * Refresh the user's details
+     *
+     */
+    $('#inpsyde-refresh-details').on('click', function ( e ) {
 
-		e.preventDefault();
-		// Get the user ID
-		let userId      = $( this ).data( 'id' );
-		let userWrapper = $( '#inpsyde-user-details' );
+        e.preventDefault();
+        // Get the user ID
+        let userId      = $(this).data('id');
+        let userWrapper = $('#inpsyde-user-details');
 
-		// Wrong ID
-		if ( !Number.isInteger( userId ) ) {
-			swal.fire( inpsyde.i18n.error, inpsyde.i18n.invalid_id, 'error' );
-			return;
-		}
+        // Wrong ID
+        if ( !Number.isInteger(userId) ) {
+            swal.fire(inpsyde.i18n.error, inpsyde.i18n.invalid_id, 'error');
+            return;
+        }
 
-		// Send an ajax request
-		$.ajax(
-			{
-				url: inpsyde.rest_url.user_flush,
-				method: 'DELETE',
-				data: { user_id: userId, nonce: inpsyde.nonce },
-				dataType: 'json',
-				beforeSend: function ( jqXHR, settings ) {
-					jqXHR.overrideMimeType( "text/plain; charset=x-user-defined" );
-					jqXHR.setRequestHeader( 'X-WP-Nonce', inpsyde.nonce );
+        // Send an ajax request
+        $.ajax(
+            {
+                url: inpsyde.rest_url.base + inpsyde.rest_url.user_flush,
+                method: 'DELETE',
+                data: { user_id: userId, nonce: inpsyde.nonce },
+                dataType: 'json',
+                beforeSend: function ( jqXHR, settings ) {
+                    jqXHR.overrideMimeType("text/plain; charset=x-user-defined");
+                    jqXHR.setRequestHeader('X-WP-Nonce', inpsyde.nonce);
 
-					// Block the table
-					userWrapper.block( {
-						message: '<div class="blockui "><span>' + inpsyde.i18n.processing + '</span><span><div class="spinner spinner--v2 spinner--primary "></div></span></div>',
-						css: {
-							border: '0px',
-							backgroundColor: 'transparent',
-							textAlign: 'center',
-							width: 'auto',
-						},
-						overlayCSS: {
-							backgroundColor: '#000000',
-							opacity: 0.1,
-							cursor: 'wait'
-						}
-					} );
-				},
-				success: function ( data, textStatus, jqXHR ) {
+                    // Block the table
+                    userWrapper.block({
+                        message: '<div class="blockui "><span>' + inpsyde.i18n.processing + '</span><span><div class="spinner spinner--v2 spinner--primary "></div></span></div>',
+                        css: {
+                            border: '0px',
+                            backgroundColor: 'transparent',
+                            textAlign: 'center',
+                            width: 'auto',
+                        },
+                        overlayCSS: {
+                            backgroundColor: '#000000',
+                            opacity: 0.1,
+                            cursor: 'wait'
+                        }
+                    });
+                },
+                success: function ( data, textStatus, jqXHR ) {
 
-					// Check if the server's response vas valid
-					if ( false === data.success ) {
-						swal.fire( inpsyde.i18n.error, data.message, 'error' );
-						return;
-					}
+                    // Check if the server's response vas valid
+                    if ( false === data.success ) {
+                        swal.fire(inpsyde.i18n.error, data.message, 'error');
+                        return;
+                    }
 
-					// Fetch a fresh copy
-					inpsydeGetUserDetails( userId );
+                    // Fetch a fresh copy
+                    inpsydeGetUserDetails(userId);
 
-				},
-				error: function ( jqXHR, textStatus, errorThrown ) {
-					// Show the error
-					swal.fire( inpsyde.i18n.error, jqXHR.responseText, 'error' );
+                },
+                error: function ( jqXHR, textStatus, errorThrown ) {
+                    // Show the error
+                    swal.fire(inpsyde.i18n.error, jqXHR.responseText, 'error');
 
-					// Hide the results
-					userWrapper.fadeOut();
-				},
-				complete: function ( jqXHR, textStatus ) {
-					// Unblock the table
-					userWrapper.unblock();
-				}
-			}
-		);
-	} );
-})( jQuery );
+                    // Hide the results
+                    userWrapper.fadeOut();
+                },
+                complete: function ( jqXHR, textStatus ) {
+                    // Unblock the table
+                    userWrapper.unblock();
+                }
+            }
+        );
+    });
+})(jQuery);
--- inpsyde.php
+++ inpsyde.php
@@ -1,31 +1,36 @@
 <?php
-	/**
-	 * Plugin Name:       InpsydeUsers
-	 * Plugin URI:        https://github.com/JackJohansson/Inpsyde-Project
-	 * Description:       A sample project assigned by Inpsyde gmbh
-	 * Version:           1.0.0
-	 * Requires at least: 5.2
-	 * Requires PHP:      7.4
-	 * Author:            Jack Johansson
-	 * Author URI:        https://jackjohansson.com/
-	 * License:           GPL v3 or later
-	 * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
-	 * Text Domain:       inpsyde-users
-	 * Domain Path:       /languages/
-	 *
-	 * @package Inpsyde
-	 */
 
-	// Plugin path and file.
-	define( 'Inpsyde\PLUGIN_DIR', __DIR__ );
-	define( 'Inpsyde\PLUGIN_FILE', __FILE__ );
-	define( 'Inpsyde\PLUGIN_URL', plugin_dir_url( Inpsyde\PLUGIN_FILE ) );
-	define( 'Inpsyde\API_ROOT', 'https://jsonplaceholder.typicode.com/' );
+/**
+ * Plugin Name:       InpsydeUsers
+ * Plugin URI:        https://github.com/JackJohansson/Inpsyde-Project
+ * Description:       A sample project assigned by Inpsyde gmbh
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.4
+ * Author:            Jack Johansson
+ * Author URI:        https://jackjohansson.com/
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       inpsyde-users
+ * Domain Path:       /languages/
+ *
+ * @package InpsydeUsers
+ */
 
-	// Require the main class.
-if ( file_exists( \Inpsyde\PLUGIN_DIR . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'Kernel.php' ) ) {
-	// Load the kernel.
-	require_once \Inpsyde\PLUGIN_DIR . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'Kernel.php';
-	// Init the plugin.
-	Inpsyde\Kernel::init();
+declare(strict_types=1);
+
+namespace Inpsyde;
+
+// Plugin path and file.
+const PLUGIN_DIR = __DIR__;
+const PLUGIN_FILE = __FILE__;
+const API_ROOT = 'https://jsonplaceholder.typicode.com/';
+define('Inpsyde\PLUGIN_URL', plugin_dir_url(PLUGIN_FILE));
+
+// Require the main class.
+if (file_exists(PLUGIN_DIR . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'Kernel.php')) {
+    // Load the kernel.
+    require_once PLUGIN_DIR . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'Kernel.php';
+    // Init the plugin.
+    Kernel::init();
 }
